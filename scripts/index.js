cf2b480f8066c514ab3c9b0d90accbf0f9bc5bed
malaikaMI <malaikamic@gmail.com>
2020-03-12 12:11:12 +0500

    added loader

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 $(window).on("load", function() {
-  $(".loader-wrapper").fadeOut(6000);
+  $(".loader-wrapper").fadeOut(3000);
 });
 $(window).scroll(function() {
   $(".head").css("opacity", 1 - $(window).scrollTop() / 950);
